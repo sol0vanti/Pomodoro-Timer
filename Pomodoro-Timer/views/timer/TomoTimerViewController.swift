45aa11f2c5cee20ef7d579a8e4e16e09e79d39fc
aiexxash <ialexballa@gmail.com>
2024-04-13 13:51:11 +0300

    chore: vc connection storyboard

---
+++ Pomodoro-Timer/views/timer/TomoTimerViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class TomoTimerViewController: UIViewController {
+    @IBOutlet weak var tomoButton: UIButton!
+    @IBOutlet weak var timerButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var startButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func tomoButtonClicked(_ sender: UIButton) {}
+    @IBAction func timerButtonClicked(_ sender: UIButton) {}
+    @IBAction func stopButtonClicked(_ sender: UIButton) {}
+    @IBAction func startButtonClicked(_ sender: UIButton) {}
+}
